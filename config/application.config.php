f9fdca2a1ee6eee4e89c0164613cc71171db5a7c
Thiago Xavier <thiagoxavier123@gmail.com>
2017-04-11 00:56:45 -0300

    Atualização

--- config/application.config.php
+++ config/application.config.php
@@ -8,7 +8,10 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
+        'Doctrinemodule',
+        'DoctrineORMModule',
         'Base',
+        'GMUD',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
